e82ccd48edc20a1dbf07ea6de599fe287e8f1e66
ilnoumri <ilnoumri@gmail.com>
2021-12-16 02:55:12 +0100

    Optimizing day15

--- day15.py
+++ day15.py
@@ -1,6 +1,7 @@
 from lib import open_file
 from collections import defaultdict
 from queue import PriorityQueue
+from functools import lru_cache
 
 lines = open_file("input/day15.txt")
 graph = []
@@ -9,7 +10,8 @@ for line in lines:
     costs = [int(e) for e in line]
     graph.append(costs)
 
-def get_adj(graph, loc):
+@lru_cache
+def get_adj(width, height, loc):
     i, j = loc
     top = (i, j-1)
     bottom = (i, j+1)
@@ -19,19 +21,19 @@ def get_adj(graph, loc):
     res = []
     for direction in directions:
         x, y = direction
-        if x < 0 or x >= len(graph[0]) or y < 0 or y >= len(graph):
+        if x < 0 or x >= width or y < 0 or y >= height:
             continue
-        res.append(direction)
-    return res
+        yield direction
 
 def dijkstra(graph, org):
+    width, height = len(graph[0]), len(graph)
     q = PriorityQueue()
     dist = defaultdict(int)
     dist[org] = 0
     q.put((dist[org], org))
     while not q.empty():
         _, current = q.get()
-        for adj in get_adj(graph, current):
+        for adj in get_adj(width, height, current):
             x, y = adj
             new_dist = dist[current] + graph[x][y]
             if adj not in dist or dist[adj] > new_dist:
